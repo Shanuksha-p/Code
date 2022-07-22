ff39d6c5a9e4b0e4f5afc88436fde546daecf467
Shanuksha Preeti <shanuksha.p@turvo.com>
2022-07-22 08:54:17 +0530

    Finding intersection

--- src/main/java/Leetcode/Topics/BinarySearch/IntersectionOfThreeSortedArrays.java
+++ src/main/java/Leetcode/Topics/BinarySearch/IntersectionOfThreeSortedArrays.java
@@ -1,7 +1,37 @@
 package Leetcode.Topics.BinarySearch;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer>ans=new ArrayList<>();
+        int i=0, j=0, k=0;
+        while(i<arr1.length && j<arr2.length && k<arr3.length){
+            if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+                ans.add(arr1[i]);
+                i++;j++;k++;
+            }
+            else if(arr1[i]<arr2[j])
+                i++;
+            else if(arr2[j]<arr3[k])
+                j++;
+            else
+                k++;
+        }
+        return ans;
+    }
+}
+
 public class IntersectionOfThreeSortedArrays {
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        Solution solution=new Solution();
+        int arr1[] = {1,2,3,4,5};
+        int arr2[] = {1,2,5,7,9};
+        int arr3[]= {1,3,4,5,8};
+        List<Integer> result= solution.arraysIntersection(arr1,arr2,arr3);
+        for(int e: result){
+            System.out.print(e + " ");
+        }
     }
 }
